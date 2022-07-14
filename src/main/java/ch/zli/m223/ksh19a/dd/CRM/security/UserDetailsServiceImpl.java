7e7efa4eb22e1e9ac1cc23b475240a240ffb3104
DavideDiGiovanni18 <digiovannid@bzz.ch>
2022-07-14 09:31:50 +0200

    Github Test 2

--- src/main/java/ch/zli/m223/ksh19a/dd/CRM/security/UserDetailsServiceImpl.java
+++ src/main/java/ch/zli/m223/ksh19a/dd/CRM/security/UserDetailsServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import ch.zli.m223.ksh19a.dd.CRM.repository.UserRepository;
 
-//TEST
+//TEST 2
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
